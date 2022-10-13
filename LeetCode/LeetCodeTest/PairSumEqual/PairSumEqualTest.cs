4a25c2d979deff60a82549e052b571fab4e3f372
Pon Karthik Ponraj <pponraj@loandepot.com>
2022-10-12 22:51:34 -0500

    pair sum equal added

---
+++ LeetCode/LeetCodeTest/PairSumEqual/PairSumEqualTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTest.PairSumEqual
+{
+    public class PairSumEqualTest
+    {
+        [Theory]
+        [ClassData(typeof(PairSumEqualTestData))]
+        public void PairSumEqualInitialTest(int[] input, int target, int[] expeted)
+        {
+            PairSumEqual pairSumEqual = new();
+            var actual = pairSumEqual.PairSumEqualFn(input, target);
+            actual.Should().Equal(expeted);
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/RunningSumOf1D/RunningSumOf1DTestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTest.RunningSumOf1D
+{
+    internal class RunningSumOf1DTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { new[] { 1, 2, 3, 4 }, new[] { 1, 3, 6, 10 } };
+            yield return new object[] { new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 3, 4, 5 } };
+            yield return new object[] { new[] { 3, 1, 2, 10, 1 }, new[] { 3, 4, 6, 16, 17 } };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
