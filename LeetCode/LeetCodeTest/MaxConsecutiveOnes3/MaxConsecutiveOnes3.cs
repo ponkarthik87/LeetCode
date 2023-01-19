8bc878bb0aacd67c0450177a1d1f3300bf762a1a
Pon Karthik Ponraj <pponraj@loandepot.com>
2023-01-18 22:45:26 -0600

    Added MaxConsecutiveOnes3

--- LeetCode/LeetCodeTest/MaxConsecutiveOnes3/MaxConsecutiveOnes3.cs
+++ LeetCode/LeetCodeTest/MaxConsecutiveOnes3/MaxConsecutiveOnes3.cs
@@ -15,6 +15,7 @@ namespace LeetCodeTest.MaxConsecutiveOnes3
             var length = 0;
             var zeroCounter = 0;
             var currentPosition = 0;
+            var isFirstRun = true;
             var zeroPositions = new List<int>();
             while (right < nums.Length)
             {
@@ -22,13 +23,20 @@ namespace LeetCodeTest.MaxConsecutiveOnes3
                 {
                     zeroPositions.Add(right);
                     zeroCounter++;
-                    if (zeroCounter == k + 1)
+                    if (zeroCounter == k + 1 || right == nums.Length - 1)
                     {
-                        length = Math.Max(length, (right - left));
+                        var addValue = isFirstRun ? 0 : 1;
+                        addValue = right == nums.Length - 1 && isFirstRun ? -1 : addValue;
+                        length = Math.Max(length, (right - (left + (addValue))));
+                        isFirstRun = false;
                         left = zeroPositions[currentPosition];
                         currentPosition++;
                         zeroCounter--;
                     }
+                }
+                else if (right == nums.Length - 1)
+                {
+                    length = Math.Max(length, (right - (left + (isFirstRun ? -1 : 0))));
                 }
 
                 right++;
--- LeetCode/LeetCodeTest/MaxConsecutiveOnes3/MaxConsecutiveOnes3TestData.cs
+++ LeetCode/LeetCodeTest/MaxConsecutiveOnes3/MaxConsecutiveOnes3TestData.cs
@@ -13,6 +13,11 @@ namespace LeetCodeTest.MaxConsecutiveOnes3
         {
             yield return new object[] { new[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, 2, 6 };
             yield return new object[] { new[] { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 }, 3, 10 };
+            yield return new object[] { new[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 2, 16 };
+            yield return new object[] { new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }, 1, 11 };
+            yield return new object[] { new[] { 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 }, 1, 11 };
+            yield return new object[] { new[] { 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0 }, 2, 11 };
+            // yield return new object[] { new[] { 1, 1 }, 4, 0 };
         }
 
         IEnumerator IEnumerable.GetEnumerator()
